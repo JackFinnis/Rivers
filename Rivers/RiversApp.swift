ab51fe0d6199f18c36708463f6b258f2d6791d0b
Jack Finnis <finnis.j@etoncollege.org.uk>
2022-05-30 12:03:20 +0100

    Initial Commit

---
+++ Rivers/RiversApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiversApp.swift
+//  Rivers
+//
+//  Created by Jack Finnis on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct RiversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
